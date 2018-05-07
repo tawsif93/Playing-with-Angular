b742a6af9fbd199a8bb158a0761d699fc00be24f
tawsif93 <tawsif93@gmail.com>
2018-05-08 03:11:46 +0600

    htaccess

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,8 +18,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes , { useHash: true})],
   exports: [RouterModule],
-  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
+  // providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
 export class AppRoutingModule {}
